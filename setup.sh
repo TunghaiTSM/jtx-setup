eeb9b12ec11360e28c28baed3dd89aa36dd7c10a
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-02-19 13:55:33 +0800

    Update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-apt update
-apt -y install php-common composer php-gd nodejs curl
+apt update && apt install curl
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash
+apt -y install php-common  composer nodejs npm curl \
+		openssl php-bcmath php-curl php-json php-mbstring php-mysql php-tokenizer php-xml php-zip php-gd
 snap install docker
 snap install phpstorm --classic
 docker run --rm -p 3306:3306 --name=jtx-mysql -eMYSQL_ROOT_PASSWORD=root -d mysql:latest
