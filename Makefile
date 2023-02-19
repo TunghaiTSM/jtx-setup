6eb001f3d4b1cb72d42901cc9012a1142f49198e
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-02-19 16:41:46 +0800

    Update

--- Makefile
+++ Makefile
@@ -2,7 +2,6 @@ build:
 	cp .env ~/.env
 	sudo bash setup.sh
 	git clone https://$(USERNAME):$(TOKEN)@github.com/$(USERNAME)/JTX.git
-	mysql -u root --host=127.0.0.1 -P 3306 -proot -e "CREATE DATABASE jtx"
 	cd JTX && cp ~/.env ./.env \
 		&& npm install && composer install \
 		&& php artisan key:generate \
--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ curl -s https://deb.nodesource.com/setup_16.x | sudo bash
 apt update
 apt -y install php-common  composer nodejs curl mysql-client \
 		openssl php-bcmath php-curl php-json php-mbstring php-mysql php-tokenizer php-xml php-zip php-gd
-snap install docker
+snap install docker &
 snap install phpstorm --classic
 docker run --rm -p 3306:3306 --name=jtx-mysql -eMYSQL_ROOT_PASSWORD=root -d mysql:latest
+mysql -u root --host=127.0.0.1 -P 3306 -proot -e "CREATE DATABASE jtx"
