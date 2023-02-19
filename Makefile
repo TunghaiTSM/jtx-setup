50cada4f38b11706c867a06c7b9d35be8b23a4c1
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-02-19 13:42:31 +0800

    Update

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 build:
-	sudo ./setup.sh
+	cp .env ~/.env
+	sudo bash setup.sh
 	git clone https://$(USERNAME):$(TOKEN)@github.com/$(USERNAME)/JTX.git
 	cd JTX
+	cp ~/.env ./.env 
+	npm install && composer install
+	php artisan key:generate
+	php artisan storage:link
+	php artisan migrate:refresh --seed
 	phpstorm .
 
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+apt update
+apt -y install php-common composer php-gd nodejs curl
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash
-apt update && apt upgrade -y
-apt -y install php-common composer php-gd nodejs
 snap install docker
 snap install phpstorm --classic
 docker run --rm -p 3306:3306 --name=jtx-mysql -eMYSQL_ROOT_PASSWORD=root -d mysql:latest
