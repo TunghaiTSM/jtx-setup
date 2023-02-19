a7126486b771873f242c4e61155ef308457e4d0d
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-02-19 16:12:05 +0800

    Update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 apt install curl
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash
 apt update
-apt -y install php-common  composer nodejs curl \
+apt -y install php-common  composer nodejs curl mysql-client \
 		openssl php-bcmath php-curl php-json php-mbstring php-mysql php-tokenizer php-xml php-zip php-gd
 snap install docker
 snap install phpstorm --classic
